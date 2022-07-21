e4ae85360c654ef794adec0f03d73b2eac75a846
Andy Glover <andrew.l.glover@gmail.com>
2022-07-20 23:13:20 -0700

    add game results conditional tree

--- js/script.js
+++ js/script.js
@@ -16,10 +16,44 @@ function getComputerChoice (){
 }
 
 function playRound(playerSelection, computerSelection) {
-    return `Player selection ${playerSelection}` + 
-            `\nComputer selection ${computerSelection}`;
+
+    if (playerSelection=="rock") {
+        if (computerSelection=="rock") {
+            return "Both players chose Rock! It's a tie!";
+        }
+        else if (computerSelection=="paper") {
+            return "You lose! Paper beats Rock!";
+        }
+        else if (computerSelection=="scissors") {
+            return "You win! Rock beats Scissors!";
+        }
+    }
+    else if (playerSelection=="paper") {
+        if (computerSelection=="rock") {
+            return "You win! Paper beats Rock!";
+        }
+        else if (computerSelection=="paper") {
+            return "Both players chose Paper! It's a tie!";
+        }
+        else if (computerSelection=="scissors") {
+            return "You lose! Scissors beats Paper!";
+        }
+    }
+    else if (playerSelection=="scissors") {
+        if (computerSelection=="rock") {
+            return "You lose! Rock beats Scissors!";
+        }
+        else if (computerSelection=="paper") {
+            return "You win! Scissors beats Paper!";
+        }
+        else if (computerSelection=="scissors") {
+            return "Both players chose Scissors! It's a tie!";
+        }
+    }
+    
 }
 
+//TODO: case insensitivity with .toLowerCase() after adding prompt
 const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+    const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
