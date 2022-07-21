a4cd201d50a04d9610af8a5603e0143043cac562
Andy Glover <andrew.l.glover@gmail.com>
2022-07-20 22:52:05 -0700

    Add some logic for random computer guess

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+function getComputerChoice (){
+    let rand3 = Math.floor(Math.random()*3) + 1;
+    switch(true){
+        case (rand3 == 1):
+            return "rock";
+            break;
+        case (rand3 == 2):
+            return "paper";
+            break;
+        case (rand3 == 3):
+            return "scissors";
+            break;
+        default:
+            return "error";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    return `Player selection ${playerSelection}` + 
+            `\nComputer selection ${computerSelection}`;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
